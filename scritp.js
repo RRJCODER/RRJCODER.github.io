19ceb090a80f06e7dd3c67c42edc0524324b9e60
RRJCODER <128964799+RRJCODER@users.noreply.github.com>
2023-11-22 22:54:10 +0100

    Update scritp.js

--- scritp.js
+++ scritp.js
@@ -1,45 +1,14 @@
-let isTouchDevice = 'ontouchstart' in window || navigator.maxTouchPoints; // Verifica si es un dispositivo táctil
+const panels = document.querySelectorAll('.panel'); // Selecciona todos los elementos con la clase .panel
 
-let startX, startY;
-
-panels.forEach(panel => {
-    panel.addEventListener('click', () => {
-        removeActiveClasses();
-        panel.classList.add('active');
+panels.forEach(panel => { // Itera sobre cada elemento .panel
+    panel.addEventListener('click', () => { // Agrega un evento de clic a cada panel
+        removeActiveClasses(); // Llama a la función removeActiveClasses para eliminar la clase 'active' de todos los paneles
+        panel.classList.add('active'); // Agrega la clase 'active' solo al panel actual que se hizo clic
     });
-
-    if (isTouchDevice) {
-        panel.addEventListener('touchstart', (e) => {
-            startX = e.touches[0].clientX;
-            startY = e.touches[0].clientY;
-        });
-
-        panel.addEventListener('touchmove', (e) => {
-            if (!startX || !startY) {
-                return;
-            }
-
-            let currentX = e.touches[0].clientX;
-            let currentY = e.touches[0].clientY;
-            let diffX = startX - currentX;
-            let diffY = startY - currentY;
-
-            if (Math.abs(diffX) > Math.abs(diffY)) {
-                if (diffX > 0) {
-                    // Deslizar hacia la izquierda, podrías hacer algo aquí si es necesario
-                } else {
-                    // Deslizar hacia la derecha, podrías hacer algo aquí si es necesario
-                }
-            } else {
-                if (diffY > 0) {
-                    // Deslizar hacia arriba, podrías hacer algo aquí si es necesario
-                } else {
-                    // Deslizar hacia abajo, podrías hacer algo aquí si es necesario
-                }
-            }
-
-            startX = null;
-            startY = null;
-        });
-    }
 });
+
+function removeActiveClasses() { // Función para eliminar la clase 'active' de todos los paneles
+    panels.forEach(panel => { // Itera sobre cada elemento .panel
+        panel.classList.remove('active'); // Elimina la clase 'active' de cada panel
+    });
+}
